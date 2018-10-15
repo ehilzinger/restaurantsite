4b0936be00b827a9048302e7759cf3bf3a839b0c
Enzo <en.hzg53@gmail.com>
2018-10-15 16:54:31 +0200

    Neue Features: - Speisekarte ist nun implementiert, jedoch nur mit Dummy-Texten, Verlinkung in Navbar ist hinzugefügt - Es wurde ein Button hinzugefügt, um ohne viel Scrollen zurück zum Seitenanfang zu kommen, inklusive CSS
    Nutzung:
    - Button: bei "onclick" die Funktion registrieren, die unter 
    /assets/js/top_button.js liegt ("backToTop()"), button id muss 
    "scrollButton" sein.
    
    - CSS für Scroll-Button kann unter /assets/css/topbutton.css global 
    geändert werden

---
+++ assets/js/top_button.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollButton").style.display = "block";
+    } else {
+        document.getElementById("scrollButton").style.display = "none";
+    }
+}
+
+function backToTop() {
+    document.documentElement.scrollTop = 0;
+}
