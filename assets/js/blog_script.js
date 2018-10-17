230a767072440a7e80cff3c097799004021c51bb
Tim Bayer <timbayer98@gmx.de>
2018-10-17 12:03:57 +0200

    Einträge in DOM integriert

--- assets/js/blog_script.js
+++ assets/js/blog_script.js
@@ -1,29 +1,36 @@
 
+// $(document.getElementById("postButton")).click(function() {
+//     sendPost();
+// });
+ $(document).click(function() {
+     sendPost();
+ });
 
-window.addEventListener("load", () => {
-    let postButton = document.getElementById("postButton")
-    let postList = document.querySelector("body > main > ul");
-    let authorEingabe = document.getElementById("authorEingabe");
-    let titelEingabe =document.getElementById("titelEingabe");
-    let nachrichtEingabe = document.getElementById("nachrichtEingabe");
 
-});
-    let insertPost = (nachricht, titel, author) => {
+function insertPost(nachricht, titel, author, postList){
         let neuerPost = document.createElement("li");
-        postList.appendChild(liElement);
+        neuerPost.classList.add("blogeintrag");
+      //  postList.appendChild(neuerPost);
+        postList.insertBefore(neuerPost, postList.childNodes[0])
+
+
 
         let neuerTitel = document.createElement("h1");
         neuerTitel.textContent = titel;
         neuerPost.appendChild(neuerTitel);
 
-        let neuerAbsatz = document.createElement("br");
+        let Absatz = document.createElement("br");
+        neuerPost.appendChild(Absatz);
 
         let neueNachricht = document.createElement("p");
         neueNachricht.textContent = nachricht;
         neuerPost.appendChild(neueNachricht);
 
+          let neuerAbsatz2 = document.createElement("br");
+          neuerPost.appendChild(neuerAbsatz2);
+
         let neuerAuthor = document.createElement("p");
-        neuerAuthor.textContent = "von "+nachircht;
+        neuerAuthor.textContent = "geschrieben von "+author;
         neuerPost.appendChild(neuerAuthor);
 
         // let deleteElement = document.createElement("a");
@@ -36,14 +43,22 @@ window.addEventListener("load", () => {
         // });
     };
 
-    postButton.addEventListener("click", () => {
-
+//    postButton.addEventListener("click", () => {
+function sendPost(){
        // Memotext vom Anwender abfragen
-       if (nachrichtEingabe.value === null || titelEingabe.value === null || authorEingabe.value===null) return;
+       let postButton = document.getElementById("postButton")
+       let postList = document.querySelector("body > main > ul");
+       let authorEingabe = document.getElementById("authorEingabe");
+       let titelEingabe =document.getElementById("titelEingabe");
+       let nachrichtEingabe = document.getElementById("nachrichtEingabe");
+       if (nachrichtEingabe.value === null || titelEingabe.value === null || authorEingabe.value===null){
+         popUp();
+         return false;
+       }
        let nachricht = nachrichtEingabe.value;
        let author = authorEingabe.value;
        let titel = titelEingabe.value;
 
        // Neues Element in die HTML-Liste einfügen
-       insertPost(nachricht, titel, author);
-   });
+       insertPost(nachricht, titel, author, postList);
+   };
